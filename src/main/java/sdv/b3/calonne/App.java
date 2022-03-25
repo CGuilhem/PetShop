c2fd6e20785bac9c889bced5f8cb8f1fd913a9ff
CGuilhem <guilhemcalonne@yahoo.fr>
2022-03-25 14:54:54 +0100

    [DONE] Configuration du projet

---
+++ src/main/java/sdv/b3/calonne/App.java
@@ -0,0 +1,13 @@
+package sdv.b3.calonne;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
